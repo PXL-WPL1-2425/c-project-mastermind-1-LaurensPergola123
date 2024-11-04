5bd5bd511ec81400cb769588a268c4c36287f2fd
LaurensPergola123 <12102886@student.pxl.be>
2024-11-04 23:02:03 +0100

    Mastermind-02

--- MasterMind_PergolaLaurens/MasterMind_PergolaLaurens/MainWindow.xaml.cs
+++ MasterMind_PergolaLaurens/MasterMind_PergolaLaurens/MainWindow.xaml.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Mastermind
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> colors = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        private List<string> secretCode;
+
+        public MainWindow()
+        {
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            secretCode = GenerateSecretCode();
+            this.Title = "Mastermind - Oplossing: " + string.Join(", ", secretCode);
+        }
+
+        private List<string> GenerateSecretCode()
+        {
+            Random rand = new Random();
+            List<string> code = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                code.Add(colors[rand.Next(colors.Count)]);
+            }
+            return code;
+        }
+    }
+}
+
+     
+
+            
