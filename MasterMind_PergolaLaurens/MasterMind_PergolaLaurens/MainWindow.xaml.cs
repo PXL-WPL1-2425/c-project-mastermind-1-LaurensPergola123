588d79c40649636b2a951753f7a70ac7631167ee
LaurensPergola123 <12102886@student.pxl.be>
2024-11-15 11:14:03 +0100

    naar desktop

--- MasterMind_PergolaLaurens/MasterMind_PergolaLaurens/MainWindow.xaml.cs
+++ MasterMind_PergolaLaurens/MasterMind_PergolaLaurens/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ namespace Mastermind
 
         public MainWindow()
         {
+            InitializeComponent();
             InitializeGame();
         }
 
@@ -32,9 +33,109 @@ namespace Mastermind
             }
             return code;
         }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox && comboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string selectedColor = selectedItem.Content.ToString();
+
+                Label targetLabel = null;
+                if (comboBox == Color1) targetLabel = color1Label;
+                else if (comboBox == Color2) targetLabel = color2Label;
+                else if (comboBox == Color3) targetLabel = color3Label;
+                else if (comboBox == Color4) targetLabel = color4Label;
+
+                if (targetLabel != null)
+                {
+                    targetLabel.Background = (Brush)new BrushConverter().ConvertFromString(selectedColor);
+                }
+            }
+        }
+
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> playerGuess = new List<string>
+            {
+                (Color1.SelectedItem as ComboBoxItem)?.Content.ToString(),
+                (Color2.SelectedItem as ComboBoxItem)?.Content.ToString(),
+                (Color3.SelectedItem as ComboBoxItem)?.Content.ToString(),
+                (Color4.SelectedItem as ComboBoxItem)?.Content.ToString()
+            };
+
+            if (playerGuess.Contains(null))
+            {
+                resultTextBlock.Text = "Selecteer vier kleuren!";
+                return;
+            }
+
+            ResetLabelBorders();
+
+            int correctPosition = 0;
+            int correctColor = 0;
+
+            List<string> tempSecretCode = new List<string>(secretCode);
+            List<string> tempPlayerGuess = new List<string>(playerGuess);
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (tempPlayerGuess[i] == tempSecretCode[i])
+                {
+                    correctPosition++;
+                    tempSecretCode[i] = null;
+                    tempPlayerGuess[i] = null;
+
+                    Label label = GetLabelByIndex(i);
+                    label.BorderBrush = Brushes.DarkRed;
+                    label.BorderThickness = new Thickness(6);
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (tempPlayerGuess[i] != null && tempSecretCode.Contains(tempPlayerGuess[i]))
+                {
+                    correctColor++;
+                    tempSecretCode[tempSecretCode.IndexOf(tempPlayerGuess[i])] = null;
+
+                    Label label = GetLabelByIndex(i);
+                    label.BorderBrush = Brushes.Wheat;
+                    label.BorderThickness = new Thickness(6);
+                }
+            }
+
+            resultTextBlock.Text = $"Rode hints: {correctPosition * 2}, Witte hints: {correctColor}";
+
+            if (correctPosition == 4)
+            {
+                MessageBox.Show("Gefeliciteerd! Je hebt de code gekraakt!", "Gewonnen");
+                InitializeGame();
+            }
+        }
+
+        private void ResetLabelBorders()
+        {
+            color1Label.BorderBrush = Brushes.Transparent;
+            color2Label.BorderBrush = Brushes.Transparent;
+            color3Label.BorderBrush = Brushes.Transparent;
+            color4Label.BorderBrush = Brushes.Transparent;
+
+            color1Label.BorderThickness = new Thickness(0);
+            color2Label.BorderThickness = new Thickness(0);
+            color3Label.BorderThickness = new Thickness(0);
+            color4Label.BorderThickness = new Thickness(0);
+        }
+
+        private Label GetLabelByIndex(int index)
+        {
+            return index switch
+            {
+                0 => color1Label,
+                1 => color2Label,
+                2 => color3Label,
+                3 => color4Label,
+                _ => null
+            };
+        }
     }
 }
-
-     
-
-            
